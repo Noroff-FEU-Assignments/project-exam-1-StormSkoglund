4d7c5a3f453a7ec35036cae203804bfd2b4bf008
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2023-11-07 18:32:42 +0100

    add new css file, recipe.list.css. Image and title fetched in recipe-list.js

---
+++ JavaScript/recipe-list.js
@@ -0,0 +1,23 @@
+const recipeList = document.querySelector(".recipe-container");
+
+const url = "https://sem-exam-fall23.alex-skoglund.no//wp-json/wp/v2/posts";
+
+async function retrieveRecipes() {
+  const response = await fetch(url);
+  const recipes = await response.json();
+  return recipes;
+  console.log(recipes);
+}
+
+async function renderRecipes() {
+  const recipePosts = await retrieveRecipes();
+  console.log(recipePosts);
+
+  const posts = recipePosts;
+
+  posts.forEach((post) => {
+    recipeList.innerHTML += `<img class="api-image" src="${post.better_featured_image.source_url}" alt="${post.better_featured_image.alt_text}" ></img><p>${post.title.rendered}</p>`; //Sorting out images from the media endpoint for rendering.
+  });
+}
+
+renderRecipes();
