db5b28897cded16ad813dfa4398c4841021c0d43
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2023-11-08 13:10:53 +0100

    add styling to api call. Recipes turned into card, consisting of images and titles!

--- JavaScript/recipe-list.js
+++ JavaScript/recipe-list.js
@@ -16,7 +16,7 @@ async function renderRecipes() {
   const posts = recipePosts;
 
   posts.forEach((post) => {
-    recipeList.innerHTML += `<img class="api-image" src="${post.better_featured_image.source_url}" alt="${post.better_featured_image.alt_text}" ></img><p>${post.title.rendered}</p>`; //Sorting out images from the media endpoint for rendering.
+    recipeList.innerHTML += `<div class="card"><img class="api-image" src="${post.better_featured_image.source_url}" alt="${post.better_featured_image.alt_text}" ></img><p>${post.title.rendered}</p></div>`; //Sorting out images from the media endpoint for rendering.
   });
 }
 
